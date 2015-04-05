2e9ce2a75dffa209b7b22f5b3bd12c43640836b9
Mykola Dimura <mykola.dimura@gmail.com>
2015-04-05 12:55:19 +0200

    Current dir is to to exe-dir only on Win.

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setApplicationVersion(APP_VERSION);
-    QDir::setCurrent(QCoreApplication::applicationDirPath());
-    MainWindow w;
 #ifdef _WIN32
     RegisterHotKey((HWND)w.winId(), 100, 0, 0x76);//F7
     RegisterHotKey((HWND)w.winId(), 101, 0, 0x75);//F6
+    QDir::setCurrent(QCoreApplication::applicationDirPath());
 #endif
+    MainWindow w;
     w.show();
 
     return a.exec();
